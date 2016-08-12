f782198e2dad6a70b3b0def704c6189409d71d4a
naayadaa <naayadaa@gmail.com>
2016-08-12 17:42:23 +0300

    Initial commit

---
+++ src/main/java/com/naya/GmapsApplication.java
@@ -0,0 +1,12 @@
+package com.naya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmapsApplication.class, args);
+	}
+}
